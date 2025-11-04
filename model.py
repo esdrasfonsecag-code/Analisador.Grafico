325e2e9d57fb49df92754fea0bd58c6932098217
Esdras <esdrasfonsecag@gmail.com>
2025-11-04 16:27:01 -0300

    Create model.py

---
+++ model.py
@@ -0,0 +1,26 @@
+import joblib
+import numpy as np
+from xgboost import XGBClassifier
+from data_handler import load_model, save_model
+
+class FootballAIPro:
+    def __init__(self):
+        self.model = load_model()
+        if self.model is None:
+            self.model = XGBClassifier(
+                n_estimators=200,
+                max_depth=4,
+                learning_rate=0.1,
+                use_label_encoder=False,
+                eval_metric='logloss'
+            )
+
+    def update_model(self, X_new, y_new):
+        """
+        Treina incrementalmente: concatena com histórico para manter aprendizado
+        """
+        self.model.fit(X_new, y_new)
+        save_model(self.model)
+
+    def predict(self, X):
+        return self.model.predict_proba(X)[:, 1]  # probabilidade de over 2.5
